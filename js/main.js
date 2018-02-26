31117c7ed48dd5cea449f355638b53697074b1f8
Nam Le <quangnamle23@gmail.com>
2018-02-26 10:40:13 +0200

    Add

--- js/main.js
+++ js/main.js
@@ -1,13 +1,16 @@
-let scene, camera, renderer;
+let scene, camera, renderer, orbitControl;
 
 function display3dModel(OBJ_PATH, OBJ_NAME, MTL_PATH, MTL_NAME) {
     scene = new THREE.Scene();
 
     camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight);
 
+    orbitControl = new THREE.OrbitControl(camera);
+
     renderer = new THREE.WebGLRenderer({
         alpha: true
     });
+
     renderer.setClearColor(new THREE.Color(0x000, 1.0));
     renderer.setSize(window.innerWidth, window.innerHeight);
     renderer.shadowMap.enabled = true;
@@ -24,16 +27,19 @@ function display3dModel(OBJ_PATH, OBJ_NAME, MTL_PATH, MTL_NAME) {
     spotLight.intensity = 1;
     scene.add(spotLight);
 
+    let clock = THREE.Clock();
+    let delta = clock.getDelta();
+
     document.getElementById('WebGL').appendChild(renderer.domElement);
 
-    var mtlLoader = new THREE.MTLLoader();
+    let mtlLoader = new THREE.MTLLoader();
     mtlLoader.setTexturePath(MTL_PATH);
     mtlLoader.setPath(MTL_PATH);
     mtlLoader.load(MTL_NAME, function (materials) {
 
         materials.preload();
 
-        var objLoader = new THREE.OBJLoader();
+        let objLoader = new THREE.OBJLoader();
         objLoader.setMaterials(materials);
         objLoader.setPath(OBJ_PATH);
         objLoader.load(OBJ_NAME, function (object) {
@@ -48,14 +54,20 @@ function display3dModel(OBJ_PATH, OBJ_NAME, MTL_PATH, MTL_NAME) {
 
     function render() {
         renderer.autoClear = false;
+        orbitControl.update(delta);
         requestAnimationFrame(render);
     }
 }
 
 //load image for wallpaper
 function loadImage(imageURL) {
-    document.body.style.backgroundImage = "url(" + "imageURL" + ")";
+    document.body.style.backgroundImage = "url(" + imageURL + ")";
 }
+
+//reset page
+document.getElementById('btnReset').addEventListener('click', function () {
+    location.reload();
+});
 
 window.onload = display3dModel;
 window.onload = loadImage;
