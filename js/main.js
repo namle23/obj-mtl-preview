1ff289797811a34ab8969163050cc9d588af96a8
Nam Le <quangnamle23@gmail.com>
2018-02-27 05:56:24 +0200

    STL

--- js/main.js
+++ js/main.js
@@ -1,88 +1,96 @@
-let scene, camera, renderer, orbitControl;
+let scene, camera, renderer;
 
-function display3dModel(OBJ_PATH, OBJ_NAME, MTL_PATH, MTL_NAME) {
+function loadFileFromURL(url) {
+    var fileInput = document.getElementById('fileInput');
+
+    fileInput.addEventListener('change', function (e) {
+
+        // file selection is done you can now read the file
+        var file = this.files[0];
+
+        // set your file encoding
+        var encoding = 'ISO-8859-1';
+
+        // create a file reader
+        var reader = new FileReader();
+
+        // set on load handler for reader
+        reader.onload = function (e) {
+            var result = reader.result;
+
+            // parse using your corresponding loader
+            loader.parse(result);
+        }
+
+        // read the file as text using the reader
+        reader.readAsText(file, encoding);
+
+    });
+}
+
+function load3D() {
+
     scene = new THREE.Scene();
-
-    camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight);
-
-    orbitControl = new THREE.OrbitControl(camera);
-
+    camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 0.1, 1000);
+    // create render
     renderer = new THREE.WebGLRenderer({
         alpha: true
     });
-
     renderer.setClearColor(new THREE.Color(0x000, 1.0));
     renderer.setSize(window.innerWidth, window.innerHeight);
     renderer.shadowMap.enabled = true;
 
-    camera.position.x = 50;
+    let loader = new THREE.STLLoader();
+    let group = new THREE.Object3D();
+    loader.load("http://www.cc.puv.fi/~tka/2018/nam/obj-mtl-preview/Test2.stl", function (geometry) {
+        console.log(geometry);
+        let mat = new THREE.MeshLambertMaterial({ color: 0x7777ff });
+        group = new THREE.Mesh(geometry, mat);
+        group.rotation.x = -0.5 * Math.PI;
+        group.scale.set(0.6, 0.6, 0.6);
+        scene.add(group);
+    });
+
+    // position the camera 
+    camera.position.x = 100;
     camera.position.y = 80;
-    camera.position.z = 40;
+    camera.position.z = 100;
+
+    //declare orbitControl, renderer.domElement prevent mouse effect in dat.GUI area
+    let orbitControls = new THREE.OrbitControls(camera, renderer.domElement);
 
     let ambientLight = new THREE.AmbientLight(0x383838);
     scene.add(ambientLight);
 
+    // add spotlight
     let spotLight = new THREE.SpotLight(0xffffff);
     spotLight.position.set(300, 300, 300);
-    spotLight.intensity = 1;
+    spotLight.intensity = 0.8;
     scene.add(spotLight);
 
-    let clock = THREE.Clock();
-    let delta = clock.getDelta();
-
-    document.getElementById('WebGL').appendChild(renderer.domElement);
-
-    let mtlLoader = new THREE.MTLLoader();
-    mtlLoader.setTexturePath('./female02.mtl');
-    mtlLoader.setPath('./female02.mtl');
-    mtlLoader.load('./female02.mtl', function (materials) {
-
-        materials.preload();
-
-        let objLoader = new THREE.OBJLoader();
-        objLoader.setMaterials(materials);
-        objLoader.load('./female02.obj', function (object) {
-
-            scene.add(object);
-            object.position.y -= 60;
-
-        });
-    });
-
-    // let stlLoader = new THREE.STLLoader();
-    // let mesh = new THREE.Object3D();
-    // loader.load("./Test2.stl", function (geometry) {
-    //     let mat = new THREE.MeshLambertMaterial({ color: 0x7777ff });
-    //     mesh = new THREE.Mesh(geometry, mat);
-    //     mesh.scale.set(0.6, 0.6, 0.6);
-    //     scene.add(group);
-    // });
+    // append renderer output to HTML
+    document.getElementById("WebGL").appendChild(renderer.domElement);
 
     render();
-
     function render() {
         renderer.autoClear = false;
-        orbitControl.update(delta);
-        requestAnimationFrame(render);
+        window.requestAnimationFrame(render);
+        renderer.render(scene, camera);
+        orbitControls.update();
     }
 }
 
-//load image for wallpaper
-function loadImage(imageURL) {
-    document.body.style.backgroundImage = "url(" + imageURL + ")";
-}
-
-//reset page
-document.getElementById('btnReset').addEventListener('click', function () {
-    location.reload();
-});
-
-window.onload = display3dModel;
-window.onload = loadImage;
-
-window.addEventListener('resize', function () {
+function onResize() {
     camera.aspect = window.innerWidth / window.innerHeight;
     camera.updateProjectionMatrix();
-    renderer.setResize(window.innerWidth, window.innerHeight);
-}, false);
+    renderer.setSize(window.innerWidth, window.innerHeight);
+}
 
+function resetPage() {
+    document.getElementById('resetPage').addEventListener('click', function () {
+        location.reload();
+    }, false);
+}
+
+window.onload = load3D;
+window.addEventListener('resize', onResize, false);
