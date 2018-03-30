0767aa6dc6bc0bf1fdf3086bc84f29854773aeda
Nam Lê <quangnamle23@gmail.com>
2018-03-30 13:35:42 +0300

    Add obj loader

--- js/main.js
+++ js/main.js
@@ -1,54 +1,33 @@
-let scene, camera, renderer;
-
-function loadFileFromURL(url) {
-    var fileInput = document.getElementById('fileInput');
-
-    fileInput.addEventListener('change', function (e) {
-
-        // file selection is done you can now read the file
-        var file = this.files[0];
-
-        // set your file encoding
-        var encoding = 'ISO-8859-1';
-
-        // create a file reader
-        var reader = new FileReader();
-
-        // set on load handler for reader
-        reader.onload = function (e) {
-            var result = reader.result;
-
-            // parse using your corresponding loader
-            loader.parse(result);
-        }
-
-        // read the file as text using the reader
-        reader.readAsText(file, encoding);
-
-    });
-}
+let scene, camera, renderer, orbitControls;
 
 function load3D() {
 
     scene = new THREE.Scene();
     camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 0.1, 1000);
     // create render
-    renderer = new THREE.WebGLRenderer({
-        alpha: true
-    });
+    renderer = new THREE.WebGLRenderer({alpha: true});
     renderer.setClearColor(new THREE.Color(0x000, 1.0));
     renderer.setSize(window.innerWidth, window.innerHeight);
     renderer.shadowMap.enabled = true;
 
-    let loader = new THREE.STLLoader();
-    let group = new THREE.Object3D();
-    loader.load("http://www.cc.puv.fi/~tka/2018/nam/obj-mtl-preview/Test2.stl", function (geometry) {
-        console.log(geometry);
+    let stlLoader = new THREE.STLLoader();
+    let stlMesh = new THREE.Object3D();
+    let objLoader=new THREE.OBJLoader();
+
+    //load this if mesh is OBJ
+    // objLoader.load('', function (geometry) {
+    //    let mat=new THREE.MeshBasicMaterial({ color:0x7777ff });
+    //    objMesh=new THREE.Mesh(geometry, mat);
+    //    stlMesh.scale.set(0.6,0.6,0.6);
+    //    scene.add(objMesh);
+    // });
+
+    //load this if mesh is STL
+    stlLoader.load('asset/Test2.stl', function (geometry) {
         let mat = new THREE.MeshLambertMaterial({ color: 0x7777ff });
-        group = new THREE.Mesh(geometry, mat);
-        group.rotation.x = -0.5 * Math.PI;
-        group.scale.set(0.6, 0.6, 0.6);
-        scene.add(group);
+        stlMesh = new THREE.Mesh(geometry, mat);
+        stlMesh.scale.set(0.6, 0.6, 0.6);
+        scene.add(stlMesh);
     });
 
     // position the camera 
@@ -56,8 +35,7 @@ function load3D() {
     camera.position.y = 80;
     camera.position.z = 100;
 
-    //declare orbitControl, renderer.domElement prevent mouse effect in dat.GUI area
-    let orbitControls = new THREE.OrbitControls(camera, renderer.domElement);
+    orbitControls = new THREE.OrbitControls(camera, renderer.domElement);
 
     let ambientLight = new THREE.AmbientLight(0x383838);
     scene.add(ambientLight);
@@ -84,12 +62,6 @@ function onResize() {
     camera.aspect = window.innerWidth / window.innerHeight;
     camera.updateProjectionMatrix();
     renderer.setSize(window.innerWidth, window.innerHeight);
-}
-
-function resetPage() {
-    document.getElementById('resetPage').addEventListener('click', function () {
-        location.reload();
-    }, false);
 }
 
 window.onload = load3D;
